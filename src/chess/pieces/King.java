8fb7ccb5795b5702a338b3879daeb1f5b879ff47
michelle <gmichele498@gmail.com>
2024-06-05 16:41:07 -0300

    Possible moves of king and Rook

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,11 +16,33 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+
+		return piece == null || piece.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
-		
 		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+
+		checkDirection(matrix, -1, 0); // above
+		checkDirection(matrix, 1, 0); // below
+		checkDirection(matrix, 0, -1); // left
+		checkDirection(matrix, 0, 1); // right
+		checkDirection(matrix, -1, -1); // northwest
+		checkDirection(matrix, -1, 1); // northeast
+		checkDirection(matrix, 1, -1); // southwest
+		checkDirection(matrix, 1, 1); // southeast
+
 		return matrix;
 	}
-}//class King
+
+	private void checkDirection(boolean[][] matrix, int rowIncrement, int columnIncrement) {
+		Position positionAux = new Position(position.getRow() + rowIncrement, position.getColunm() + columnIncrement);
+
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			matrix[positionAux.getRow()][positionAux.getColunm()] = true;
+	}
+
+}// class King
